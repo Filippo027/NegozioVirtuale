39d32ea8d41ad19a04c23b5375fb21fa44f0576d
Filippo027 <filippomartellossi@gmail.com>
2025-09-02 15:18:18 +0200

    Update Utente.cs

--- Utente.cs
+++ Utente.cs
@@ -1,21 +1,18 @@
 using System.Collections.Generic;
 
-public class Utente
-{
+public class Utente{
     public string Username { get; private set; }
     private string password;
 
     public Carrello Carrello { get; private set; }
 
-    public Utente(string username, string password)
-    {
+    public Utente(string username, string password){
         Username = username;
         this.password = password;
         Carrello = new Carrello();
     }
 
-    public bool VerificaPassword(string pwd)
-    {
+    public bool VerificaPassword(string pwd){
         return password == pwd;
     }
 }
