09c5f73a4141d60686ccb5819af4ee4563838131
Filippo027 <filippomartellossi@gmail.com>
2025-09-02 15:16:13 +0200

    Update Prodotto.cs

--- Prodotto.cs
+++ Prodotto.cs
@@ -1,13 +1,22 @@
-public abstract class Prodotto{
-    public string Nome { get; private set; }
-    public float Prezzo { get; private set; }
-    public string Descrizione { get; private set; }
+public abstract class Prodotto : IVisualizzabile
+{
+    public string Nome { get; set; }
+    public float Prezzo { get; set; }
+    public string Descrizione { get; set; }
+    public string Categoria { get; set; }
 
-    public Prodotto(string nome, float prezzo, string descrizione){
+    public Prodotto(string nome, float prezzo, string descrizione, string categoria)
+    {
         Nome = nome;
         Prezzo = prezzo;
         Descrizione = descrizione;
+        Categoria = categoria;
     }
 
-    public abstract void Visualizza();
+    public abstract void MostraDettagli();
+
+    public void Visualizza()
+    {
+        MostraDettagli();
+    }
 }
